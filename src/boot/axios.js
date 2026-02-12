7ff66a1cf9c80d7fcbc1c1da9ac5427c61dc3b5d
perudev3 <peru.dev3@gmail.com>
2026-02-12 09:23:22 -0500

    integracion de ruta del servidor

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,8 @@ import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api'
+  //baseURL: 'http://127.0.0.1:8000/api'
+  baseURL: 'https://srv1364430.hstgr.cloud/api'
 })
 
 export default boot(({ app }) => {
